3089fd5565b2f1e239af41413526a793f63cb0d0
Cass Valladares <jhumphriesent@gmail.com>
2020-10-16 15:22:30 -0500

    Updates

---
+++ precache-manifest.8c35f76e257a2eea63b1bbced1667b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01088bd5962989f8c843f7af0f6476a4",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "9ea01cc6f3417b949c99",
+    "url": "/project-4-assessment/static/css/main.10c0e39f.chunk.css"
+  },
+  {
+    "revision": "608118624ae7ee197d4b",
+    "url": "/project-4-assessment/static/js/2.e208ecfd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-4-assessment/static/js/2.e208ecfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ea01cc6f3417b949c99",
+    "url": "/project-4-assessment/static/js/main.4461f91a.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
